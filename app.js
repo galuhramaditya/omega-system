05642d7d51c7303a678bf763da620e90c9b613a1
galuh ramaditya <galuh.ramaditya@gmail.com>
2019-09-28 11:02:36 +1400

    fix htdocs

--- app.js
+++ app.js
@@ -1,26 +1,46 @@
 var app = new Vue({
-  el: "#app",
-  data: {
-    cards: [
-      {
-        title: "Approval",
-        link: "http://system.omega.approval.loc",
-        img: "/assets/img/Capture3.PNG"
-      },
-      {
-        title: "Report Front Office",
-        link: "http://system.omega.front-office.loc",
-        img: "/assets/img/Capture1.PNG"
-      },
-      {
-        title: "Report Back Office",
-        link: "http://system.omega.back-office.loc",
-        img: "/assets/img/Capture.PNG"
-      }
-    ]
-  }
+    el: "#app",
+    data: {
+        cards: {
+            approval: {
+                title: "Approval",
+                link: {
+                    vhost: "http://system.omega.approval.loc",
+                    htdocs:
+                        "http://localhost/omega-package/omega-approval/public"
+                },
+                img: "assets/img/Capture3.PNG"
+            },
+            front_office: {
+                title: "Report Front Office",
+                link: {
+                    vhost: "http://system.omega.front-office.loc",
+                    htdocs:
+                        "http://localhost/omega-package/omega-front-office/public"
+                },
+                img: "assets/img/Capture1.PNG"
+            },
+            back_office: {
+                title: "Report Back Office",
+                link: {
+                    vhost: "http://system.omega.back-office.loc",
+                    htdocs:
+                        "http://localhost/omega-package/omega-back-office/public"
+                },
+                img: "assets/img/Capture.PNG"
+            }
+        }
+    },
+    methods: {
+        getLink(app) {
+            if (document.location.pathname.split("/").length < 3) {
+                return this.cards[app].link.vhost;
+            }
+            return this.cards[app].link.htdocs;
+        }
+    }
 });
 
 $(document).ready(function() {
-  $("[vue-data]").fadeIn("slow");
+    $("[vue-data]").fadeIn("slow");
 });
